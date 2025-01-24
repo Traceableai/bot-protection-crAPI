859cb15edbfc5dd89a07832444d74eb783b88324
sunilgattupalle <sunil.gattupalle@traceable.ai>
2025-01-24 15:49:28 -0800

    Templatize sitekey, captchaurl for bot protection of login flow

---
+++ deploy/publish/publish-crapiweb-to-dockerrepo-2.sh
@@ -0,0 +1,6 @@
+set +x
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/p9q5a9f9
+./services/web/build-image.sh
+
+docker tag crapi/crapi-web:latest public.ecr.aws/p9q5a9f9/traceable/bot-protection-crapi-web:latest
+docker push public.ecr.aws/p9q5a9f9/traceable/bot-protection-crapi-web:latest
---
+++ deploy/publish/publish-crapiweb-to-dockerrepo.sh
@@ -0,0 +1,8 @@
+set +x
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 141020914838.dkr.ecr.us-west-2.amazonaws.com
+
+./services/web/build-image.sh
+
+docker tag crapi-web:latest 141020914838.dkr.ecr.us-west-2.amazonaws.com/docker-dev/traceable/bot-protection-crapi-web:latest
+docker push 141020914838.dkr.ecr.us-west-2.amazonaws.com/docker-dev/traceable/bot-protection-crapi-web:latest
+
